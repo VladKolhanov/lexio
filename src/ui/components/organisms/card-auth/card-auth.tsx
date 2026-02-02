153ced9a844473b2ed7836ea628e5bc15ed6da69
VladKolhanov <vladi.kolganov@gmail.com>
2026-02-02 19:50:08 +0200

    ✨ feat: add card-auth component

---
+++ src/ui/components/organisms/card-auth/card-auth.tsx
@@ -0,0 +1,64 @@
+import { cn } from '@/shared/utils/cn'
+import { Button } from '@/ui/components/atoms/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/ui/components/atoms/card'
+import { SeparatorWithLabel } from '@/ui/components/atoms/separator'
+
+type Props = {
+  title: string
+  description: string
+  children: React.ReactNode
+
+  separatorLabel?: string
+  googleButtonText?: string
+  facebookButtonText?: string
+
+  footer?: React.ReactNode
+
+  className?: string
+}
+
+export const CardAuth = ({
+  title,
+  description,
+  children,
+  separatorLabel,
+  googleButtonText,
+  facebookButtonText,
+  footer,
+  className,
+}: Props) => {
+  const showSocials = separatorLabel && googleButtonText && facebookButtonText
+
+  return (
+    <Card className={cn('w-full sm:w-1/2 md:w-1/3', className)}>
+      <CardHeader className="mb-4 text-center">
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{description}</CardDescription>
+      </CardHeader>
+
+      <CardContent className="space-y-4">
+        {children}
+
+        {showSocials && (
+          <>
+            <SeparatorWithLabel label={separatorLabel} />
+
+            <div className="grid-col grid justify-items-center gap-2 *:w-1/2">
+              <Button>{googleButtonText}</Button>
+              <Button variant="outline">{facebookButtonText}</Button>
+            </div>
+          </>
+        )}
+      </CardContent>
+
+      {footer && <CardFooter className="justify-center">{footer}</CardFooter>}
+    </Card>
+  )
+}
